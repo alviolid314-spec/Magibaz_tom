1883916e98368cef6e8dcad58e1382465713e750
RAHI BOT BBY <rahiahmed143@gmail.com>
2026-03-15 00:05:21 +0600

    Enhance goodbye message with powered by text
    Added a powered by message to the goodbye text.

--- grpUpCmd/gdbye.js
+++ grpUpCmd/gdbye.js
@@ -4,7 +4,10 @@ module.exports = {
     const removedMembers = event.participants;
     for (const member of removedMembers) {
       await api.sendMessage(event.id, {
-        text: `Goodbye @${member.split('@')[0]}, we'll miss you!`,
+        text: `Goodbye @${member.split('@')[0]}, we'll miss you
+        
+        
+        *╰─➤ Powered by* Mᴏʜᴀᴍᴍᴀᴅ ⎯͢⎯⃝ Rꫝʜɪ__Bʜꫝɪ__ᥫᩣ!`,
         mentions: [member]
       });
     }
